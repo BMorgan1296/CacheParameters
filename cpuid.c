68a694c4dd47f329c628078c533de7cb5aae278a
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2018-03-18 02:52:43 +1030

    Program edit made, cpuid progress
    program edit will be used to change values in mastik, currently in the process of getting CPUID to return cache values. Need to work on decoding descriptors, see https://www.microbe.cz/docs/CPUID.pdf

--- cpuid.c
+++ cpuid.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
+
+void DecToHexStr(int dec, char *str)
+  {  }
 
 int main()
 {
-	int val ;
+	int32_t op = 2;  // input:  eax
+	int32_t eax; // output: eax
+	int32_t ebx; // output: ebx
+	int32_t ecx; // output: ecx
+	int32_t edx; // output: edx
 
-  asm("movl $110, %%ebx;"
-      "movl $5, %%eax;"
-      "addl %%ebx, %%eax;" :"=r" ( val )
-    );
+  asm volatile( "cpuid"
+              : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+              : "a" (op) );      
 
-  printf("\nVal: %d\n", val);
+    printf("\neax: %zu\nebx: %zu\necx: %zu\nedx: %zu\n", eax & ((1<<8)-1) ,ebx, ecx & 0xFF,edx);
+
+	  
+	for(int i = 0; i < 32; i++)
+	{
+		printf("\n%d | eax: %zu ebx: %zu ecx: %zu edx: %zu", i, eax & ((1<<i)-1), ebx & ((1<<i)-1), ecx & ((1<<i)-1), edx & ((1<<i)-1));
+	}
+	putchar('\n');
+  
 }
