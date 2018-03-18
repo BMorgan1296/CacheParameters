b7ad9e90a187dbaf9049c14a2cb066c6bc99d32b
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2018-03-18 21:23:48 +1030

    CPUID not giving the correct descriptor codes
    Will have to work on this further. Currently have one program commented out and the other for testing.

--- cpuid.c
+++ cpuid.c
@@ -1,28 +1,66 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
-void DecToHexStr(int dec, char *str)
-  {  }
-
-int main()
+int main() //http://www.sandpile.org/x86/cpuid.htm and also https://www.scss.tcd.ie/Jeremy.Jones/CS4021/processor-identification-cpuid-instruction-note.pdf
 {
 	int32_t op = 2;  // input:  eax
 	int32_t eax; // output: eax
 	int32_t ebx; // output: ebx
 	int32_t ecx; // output: ecx
 	int32_t edx; // output: edx
+    
+	__asm__ volatile(
+			"cpuid;" //  call cpuid instruction
+			:"=a"(eax),"=b"(ebx),"=c"(ecx), "=d"(edx)// output equal to "movl  %%eax %1"
+			:"a"(op)// input equal to "movl %1, %%eax"
+			//:"%eax","%ebx","%ecx","%edx"// clobbered register
+			);
 
-  asm volatile( "cpuid"
-              : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-              : "a" (op) );      
+	printf("\nShift: %d  |  eax: %X   ebx: 0x%08X   ecx: 0x%08X   edx: 0x%08X", i, (eax & ((1<<i)-1)), (ebx & ((1<<i)-1)), (ecx & ((1<<i)-1)), (edx & ((1<<i)-1)));
 
-    printf("\neax: %zu\nebx: %zu\necx: %zu\nedx: %zu\n", eax & ((1<<8)-1) ,ebx, ecx & 0xFF,edx);
-
-	  
-	for(int i = 0; i < 32; i++)
+	int i;
+	for(i = 32; i >= 0; i--)
 	{
-		printf("\n%d | eax: %zu ebx: %zu ecx: %zu edx: %zu", i, eax & ((1<<i)-1), ebx & ((1<<i)-1), ecx & ((1<<i)-1), edx & ((1<<i)-1));
+		//printf("\nShift: %d  |  eax: %zu   ebx: %zu   ecx: %zu   edx: %zu", i, (unsigned long)(eax & ((1<<i)-1)), (unsigned long)(ebx & ((1<<i)-1)), (unsigned long)(ecx & ((1<<i)-1)), (unsigned long)(edx & ((1<<i)-1)));
+		
+		printf("\nShift: %d  |  eax: 0x%08X   ebx: 0x%08X   ecx: 0x%08X   edx: 0x%08X", i, (eax & ((1<<i)-1)), (ebx & ((1<<i)-1)), (ecx & ((1<<i)-1)), (edx & ((1<<i)-1)));
+		
+		if(i % 8 == 0 && i != 0)
+		{
+			printf("\n\n------------------------ Byte: %d --------------------------\n", (i/8));
+		}
 	}
 	putchar('\n');
+	putchar('\n');
   
+}*/
+
+#include <stdio.h>
+#include <string.h>
+
+static inline void native_cpuid(unsigned int *eax, unsigned int *ebx,
+                 unsigned int *ecx, unsigned int *edx)
+{
+    
+    asm volatile("cpuid"
+        : "=a" (*eax),
+        "=b" (*ebx),
+        "=c" (*ecx),
+        "=d" (*edx)
+        : "0" (*eax), "2" (*ecx)
+        : "memory");
+}
+
+int main()
+{
+    int eax, ebx, ecx, edx;
+    eax = 0x80000006;
+    native_cpuid(&eax, &ebx, &ecx, &edx);
+    printf("\nL2 Cache Size = %d\n", (ecx & 0xFF)); //see https://www.scss.tcd.ie/Jeremy.Jones/CS4021/processor-identification-cpuid-instruction-note.pdf PAGE 47
+    printf("\n12 = %08X\n", (ecx & ((1<<12)-1)));
+    printf("\n13 = %08X\n", (edx & ((1<<13)-1)));
+    printf("\n14 = %08X\n", (edx & ((1<<14)-1)));
+    printf("\n15 = %08X\n", (edx & ((1<<15)-1)));
+    return 0;
 }
